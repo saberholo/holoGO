cbd6f8fd96184b0bd8069f15db4a4e70b7c05f9e
Ang Li <ali24@stevens.edu>
2019-10-24 18:24:53 -0400

    first try

--- start.py
+++ start.py
@@ -9,28 +9,37 @@ pygame.init()
 size = width, height = 1080, 720
 screen = pygame.display.set_mode(size)
 env = Env(9.8, screen)
-color = (0, 0, 0)
-holo = pygame.image.load('pic/holo.jpeg')
-holorect = holo.get_rect()
+BLACK = (0, 0, 0)
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode(size)
 bullet = items.StandardBullet(screen)
 
 
-speed = [1, 1]
+speed = [10, 10]
+fps = 100
 
 while True:
+    screen.fill(BLACK)
+    clock.tick(fps)
     bullet.obj = bullet.obj.move(speed)
     if bullet.obj.left < 0 or bullet.obj.right > width:
         speed[0] = -speed[0]
     if bullet.obj.top < 0 or bullet.obj.bottom > height:
         speed[1] = -speed[1]
 
-    screen.fill(color)
+
     screen.blit(bullet.surface, bullet.obj)
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        # holorect = holorect.move(speed)
+
+
+
+
+        screen.fill(BLACK)
+        pygame.draw.circle(screen, [0, 0, 0], [300, 300], 200)
 
 
 
